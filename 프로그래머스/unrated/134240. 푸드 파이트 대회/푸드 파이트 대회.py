d90330e9f92b35b18e3a433f91a0439157d76339
a-young-kim <76930260+a-young-kim@users.noreply.github.com>
2023-03-21 12:50:32 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -0,0 +1,9 @@
+def solution(food):
+    answer = ''
+    
+    for i in range(1, len(food)):
+        for j in range(0, food[i] // 2):
+            answer += str(i)
+        
+    answer = answer + '0' + answer[::-1]
+    return answer
