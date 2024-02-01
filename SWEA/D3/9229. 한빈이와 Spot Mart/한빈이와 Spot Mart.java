668f69021fbc9066d6c3b86f5e9ec91c662898e7
a-young-kim <76930260+a-young-kim@users.noreply.github.com>
2024-02-01 22:39:27 +0900

    [D3] Title: 한빈이와 Spot Mart, Time: 269 ms, Memory: 45,592 KB -BaekjoonHub

---
+++ SWEA/D3/9229. 한빈이와 Spot Mart/한빈이와 Spot Mart.java
@@ -0,0 +1,75 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// double b = 1.0;               
+// char g = 'b';
+// String var = "ABCDEFG";
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+import java.util.Scanner;
+//import java.io.FileInputStream;
+
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
+class Solution
+{
+	 static int N, M;
+    static int[] array;
+    static int answer;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
+
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            N = sc.nextInt();
+            M = sc.nextInt();
+            array = new int[N];
+            answer = Integer.MIN_VALUE;
+
+            for(int i = 0; i < N; i++){
+                array[i] = sc.nextInt();
+            }
+
+            f(0, 0, 0);
+            if(answer == Integer.MIN_VALUE) answer = -1;
+
+            System.out.println("#" + test_case + " " + answer);
+        }
+    }
+
+    public static void f(int idx, int sum, int cnt){
+        if(cnt == 2){
+            if(sum > M) return;
+            answer = Math.max(answer, sum);
+            return;
+        }
+
+        for(int i = idx; i < N; i++){
+
+            f(i + 1, sum + array[i], cnt + 1);
+        }
+    }
+}
