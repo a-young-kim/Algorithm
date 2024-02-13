ae1270a809a6803a44f0951de95240198aed6ad9
a-young-kim <76930260+a-young-kim@users.noreply.github.com>
2024-02-13 12:46:11 +0900

    [Silver I] Title: 오목, Time: 84 ms, Memory: 11752 KB -BaekjoonHub

--- 백준/Silver/2615. 오목/오목.java
+++ 백준/Silver/2615. 오목/오목.java
@@ -1,70 +1,74 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-// 왼쪽, 위를 미리 탐색하여서 탐색하였으면 제외
+/*
+ * 문제
+ * 19 * 19 크기의 바둑판
+ * 연속적으로 5알을 놓이면 그 색이 이김 -> 가로, 세로, 대각선
+ * 입력으로 바둑판의 상태가 주어졌을 때 승부 결과 출력
+ * 
+ * 입력
+ * 검은 바둑알 1, 흰바둑알 2 알이 놓이지 않는 자리 0
+ * 
+ * 출력
+ * 검은 색이 이겼을 경우1, 흰색이 이겼을 경우2 승부가 결정되지 않은 경우0
+ * 
+ * 풀이 방식
+ * 재귀로 풀음
+ * 이전 값이 현재 값과 같으면 return
+ * 세로 먼저 탐색
+ * 5개인 것을 찾으면 return
+ */
 public class Main {
-	static int[][] numArrays = new int[19][19];;
-	static int[][] visited = new int[19][19];
-	static final int[] dx = {-1, 1, 0, 0, 1, -1, -1, 1};
-	static final int[] dy = {0, 0, -1, 1, -1, 1, -1, 1};
-	
-	public static int dfs(int check, int k, int row, int col, int num, boolean setVisited) {
-		if(row < 0 || row >= 19 || col < 0 || col >= 19) return num;
+	static int[][] array = new int[19][19];
+		static int[] dx = {1, 0, 1, 1}, dy = {0, 1, 1, -1};
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 		
-		if(numArrays[row][col] == check) {
-			if(setVisited) {
-				visited[row][col] = 1;
-			}
-			num = dfs(check, k, row + dy[k], col + dx[k], num + 1, setVisited);
-		}
-			
-		return num;
-	}
-
-	public static void main(String[] args) throws Exception {
-//		System.setIn(new FileInputStream("Test5.txt"));
-		//여기에 코드를 작성하세요.
-		
-		Scanner sc = new Scanner(System.in);
-
-		for(int i = 0 ; i < 19; i++) {
-			for(int j = 0; j < 19; j++) {
-				numArrays[i][j] = sc.nextInt();
+		for(int i = 0; i < 19; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0 ;j < 19; j++) {
+				array[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		
+		int i, j = 0;
 		int answer = 0;
+		first: for(i = 0; i < 19; i++) {
+			for(j= 0; j < 19; j++) {
+				int cnt = 0;
+				if(array[j][i] == 0) continue;
+				for(int k = 0; k < 4; k++) {
 
-		// 하얀색 돌
-		first: for(int i = 0; i < 19; i++) {
-			for(int j = 0; j < 19; j++) {
-				if(numArrays[i][j] != 0) {
-					int[] setNum = new int[8];
-					// 0 1 가로 2 3세로 4 5 좌하 대각선 6 7 좌상 대각선
-					for(int k = 0; k < 8; k++) {
-						if(k % 2 == 0 && k != 0 && setNum[k] != 0) continue;
-						setNum[k] = dfs(numArrays[i][j], k, i + dy[k], j + dx[k], 0, false);
-						if(k % 2 == 1 && (setNum[k] + setNum[k - 1]) == 4) {
-							visited[i][j] = 1;
-							dfs(numArrays[i][j], k - 1, i + dy[k - 1], j + dx[k - 1], 0, true);
-							dfs(numArrays[i][j], k, i + dy[k], j + dx[k], 0, true);
-							answer = numArrays[i][j];
-							break first;
-						}
+					if(j - dy[k] >= 0 && i - dx[k] >= 0 &&  i - dx[k] < 19 && j - dy[k] < 19 &&
+							array[j - dy[k]][i - dx[k]] == array[j][i]) continue;
+					cnt = f(j, i, k, array[j][i], 0);
+					if(cnt == 5) {
+						answer = array[j][i];
+						break first;
 					}
-					
-					
 				}
 			}
 		}
-		System.out.println(answer);
-		first: for(int i = 0; i < 19; i ++) {
-			for(int j = 0; j < 19; j++) {
-				if(visited[j][i] == 1) {
-					System.out.println(j + 1 +" "+ (i + 1));
-					break first;
-				}
-			}
+
+		if(i == 19 && j == 19) System.out.println(0);
+		else {
+			System.out.println(answer);
+			System.out.println((j + 1) + " " + (i + 1));
 		}
-		sc.close();	
 	}
+	
+	public static int f(int row, int col, int flag, int state, int count) {
+		
+		
+		if(array[row][col] != state) return count;
+		count++;
+		if(row + dy[flag] >= 19 || row + dy[flag] < 0) return count;
+		if(col + dx[flag] >= 19 || col + dx[flag] < 0) return count;
+		count = f(row + dy[flag], col + dx[flag], flag, state, count);
+		return count;
+	}
+
 }
