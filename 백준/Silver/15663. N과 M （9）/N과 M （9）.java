cb0a6930c59d57b7aee6e5940c2756f013edce62
a-young-kim <76930260+a-young-kim@users.noreply.github.com>
2024-03-12 09:45:21 +0900

    [Silver II] Title: N과 M (9), Time: 2008 ms, Memory: 119164 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static int N, M;
+	static Integer[] array, answer;
+	static Set<String> set = new HashSet<>();
+	static boolean[] visited;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		array = new Integer[N];
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			array[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		answer = new Integer[M];
+		visited = new boolean[N];
+		Arrays.sort(array);
+
+		f(0, 0);
+		
+	}
+	public static void f(int cnt, int idx) {
+
+			if(cnt == M) {
+				String s = "";
+				for(int i = 0; i < M; i++) {
+					s += answer[i] + " ";
+				}
+				if(set.contains(s)) return;
+				set.add(s);
+				for(int i = 0; i < M; i++) {
+					System.out.print(answer[i] + " ");
+				}
+				System.out.println();
+				return;
+			}
+			for(int i = 0; i < N; i++) {
+				if(visited[i]) continue;
+				visited[i] = true;
+				answer[cnt] = array[i];
+				f(cnt + 1, i + 1);
+				visited[i] = false;
+			}
+	}
+	
+}
